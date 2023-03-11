ee772ecb912899c88898639659af6d6369e11f8d
Julio Augusto de Castilhos Borges <127233265+Julio-Uergs@users.noreply.github.com>
2023-03-11 15:20:38 -0300

    O EX3 FUNCIONA

--- lista1/ex3.c
+++ lista1/ex3.c
@@ -6,27 +6,30 @@
 
 void prntvctr(int vctr[size]) {
 	int i = 0;
-	for (i; i<size; i++) {
+	for (i; i < size; i++) {
 		printf("%d", vctr[i]);
-		if (i<(size-1)) {
+		if (i < (size - 1)) {
 		printf(", ");
 		}
 	}
 }
 
-int main() {
-	int vctr[size] = {0,1,2,3,4,6,0,9,5,2,7,0,70,6,8}, zros = 0, zro-index[size];
+int main() {		//0,1,2,3,4,5,6,7,8,9,10,11,12,13,14
+	int vctr[size] = {0,3,0,4,6,0,9,5,2,7, 0,70, 6,8 ,1}, swap = 0;
 	printf("Vetor original:\t {");
 	prntvctr(vctr);
 	printf("}\n");
-	for (int i = 0; i+zros == size; i++) { //mv
-		if (vctr[i] == srch){
-			
-			if (vctr[i+1] == srch) {
-			vctr[i] = vctr[i+1];
-			}
+	for (int i = 0; i < size - 1; i++) { //mv
+		if (vctr[i] == srch && vctr[i + 1] != srch){
+			swap = vctr[i];
+			vctr[i] = vctr[i + 1];
+			vctr[i + 1] = swap;
+			i = 0;
 		}
 	}
+	printf("Vetor modificado:{");
+	prntvctr(vctr);
+	printf("}\n");
 }
 
 //i + zros = size?
