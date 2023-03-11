d94ba143642764c6148445596b9eb6e6d3b672b6
Julio Augusto de Castilhos Borges <127233265+Julio-Uergs@users.noreply.github.com>
2023-03-11 13:53:18 -0300

    Preview: ex3

--- lista1/ex3.c
+++ lista1/ex3.c
@@ -1,10 +1,12 @@
-/*Crie um vetor de 15 posições de inteiros e posteriormente realize a compactação dele, isto é, elimine as posições com valor zero avançando uma posição, com os com os valores subseqüentes do vetor. Dessa forma todos “zeros” devem ficar para as posições finais do vetor.*/
+/*Crie um vetor de 15 posições de inteiros e posteriormente realize a compactação dele, isto é, elimine as posições com valor zero avançando uma posição, com os com os valores subsequentes do vetor. Dessa forma todos “zeros” devem ficar para as posições finais do vetor.*/
 
-#define size = 15
+#define size 15
+#define srch 0
 #include <stdio.h>
 
-void imprime(int vctr[size]) {
-	for (int i = 0; i<size; i++) {
+void prntvctr(int vctr[size]) {
+	int i = 0;
+	for (i; i<size; i++) {
 		printf("%d", vctr[i]);
 		if (i<(size-1)) {
 		printf(", ");
@@ -13,14 +15,21 @@ void imprime(int vctr[size]) {
 }
 
 int main() {
-	int vctr[size] = {0,1,2,3,4,6,0,9,5,2,7,0,70,6,8};
-	/*printf("Por favor, insira 15 valores inteiros, destes, os 8 primeiros serão posicionados ao final e os 8 últimos, no início.");
-	for (int i; i<16; i++) {
-		scanf(" %d", &vctr[i]);
-	}*/
+	int vctr[size] = {0,1,2,3,4,6,0,9,5,2,7,0,70,6,8}, zros = 0, zro-index[size];
 	printf("Vetor original:\t {");
-	imprime(vctr);
+	prntvctr(vctr);
 	printf("}\n");
-	for (int i = 0; i<15; i++) {
-		
+	for (int i = 0; i+zros == size; i++) { //mv
+		if (vctr[i] == srch){
+			
+			if (vctr[i+1] == srch) {
+			vctr[i] = vctr[i+1];
+			}
+		}
 	}
+}
+
+//i + zros = size?
+	//for ( enche de zero	//trim
+	/*Debug*/
+	
