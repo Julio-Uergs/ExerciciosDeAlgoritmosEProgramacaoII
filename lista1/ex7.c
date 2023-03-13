32fd3c1152cb3256dbf3f5f81b9e71c491bfb780
Julio Augusto de Castilhos Borges <127233265+Julio-Uergs@users.noreply.github.com>
2023-03-13 15:03:21 -0300

    Partial: 7

---
+++ lista1/ex7.c
@@ -0,0 +1,14 @@
+/*Leia duas matrizes 4 x 4 e escreva uma terceira com os maiores elementos entre as primeiras*/
+
+#define size 4
+
+#include <stdio.h>
+
+int main() {
+	int i = 0, j = 0, m1[size][size], m2[size][size], m3[size][size];
+	for (i = 0; i < size; i++) {
+		for (j = 0; j < size; j++) {
+			
+		}
+	}
+}
