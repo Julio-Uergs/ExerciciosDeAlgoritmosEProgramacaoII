ffa5bd1360bf05840ca7dfb598980630aed96004
Julio-Uergs <julio-borges@uergs.edu.br>
2023-03-20 22:10:39 -0300

    Feito é melhor do que perfeito

---
+++ lista2/vctr5mult.c
@@ -0,0 +1,23 @@
+/*Elabore um programa que cria um vetor de 5 posições de inteiros e utilizando uma função altere os valores do vetor lido com o valor de cada elemento do vetor pela multiplicação do número lido.*/
+
+#define size 5
+#include <stdio.h>
+
+void vctrmult (int vctr[5], int sclr) {
+    for (int i=0; i<size; i++) {
+        vctr[i] = vctr[i] * sclr;
+        printf("%d ", vctr[i]);
+    }
+}
+
+int main() {
+    int vctr[size] = {0,1,2,3,4}, sclr = 0;
+    printf("Por favor, insira um valor como escalar do vetor: ");
+    for (int i=0; i<size; i++) {
+        printf("%d ", vctr[i]);
+    }
+    printf("* ");
+    scanf("%d", &sclr);
+    printf("= ");
+    vctrmult(vctr, sclr);
+}
