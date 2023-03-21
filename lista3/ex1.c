8b5ec6247ff43ec653c5a53f818afbc71ffde7f0
Julio-Uergs <julio-borges@uergs.edu.br>
2023-03-21 14:10:29 -0300

    sem o moodle ;-;

---
+++ lista3/ex1.c
@@ -0,0 +1,19 @@
+/*implemente uma função recursiva para computar o valor de 2^n*/
+
+#include <stdio.h>
+#include <math.h>
+
+int pot(int n) {
+    if (n == 1) {
+        return 2;
+    } else {
+        return 2 * pot(n-1);
+    }
+}
+
+int main() {
+    int n = 0;
+    printf("Insira um valor para n sendo 2^n a saida do programa: ");
+    scanf("%d", &n);
+    printf("%d\n", pot(n));
+}
